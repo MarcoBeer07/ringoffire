b1ae7ab677506e91d7758682c5b2a39c487f6404
Marco <marco.beer93@web.de>
2022-06-17 15:06:55 +0200

    working  on responsive view

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
@@ -9,14 +9,23 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  @Input() responisiveView = false;
   pickCardAnimation = false;
   currentCard: string = ''
   game: Game;
+  innerHeight: any;
+  innerWidth: any;
 
-  constructor(public dialog: MatDialog) { }
+
+  constructor(public dialog: MatDialog) {
+    this.innerHeight = (window.screen.height) + "px";
+    this.innerWidth = (window.screen.width) + "px";
+
+  }
 
   ngOnInit(): void {
     this.newGame();
+
   }
 
   newGame() {
@@ -48,4 +57,19 @@ export class GameComponent implements OnInit {
     });
   }
 
+  addStyle(i) {
+    if (innerWidth > 1100) {
+      return {
+        'left.px': 40 + (i * 225)
+      }
+    } else if (innerWidth < 1100) {
+      return {
+        'left.px': 40 + (i * 60)
+      }
+    } else if (innerWidth < 500) {
+      return {
+        'left.px': 40 + (i * 50)
+      }
+    }
+  }
 }
